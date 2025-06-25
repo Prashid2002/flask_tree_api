a8a7553aa916aa93ed12c502c8a57785ebb73edd
Prashid Shanker <115932976+Prashid2002@users.noreply.github.com>
2025-06-26 00:07:27 +0530

    Update app.py

--- app.py
+++ app.py
@@ -2,15 +2,21 @@ from flask import Flask
 from flask_cors import CORS
 from routes.auth import auth_bp
 from routes.tree import tree_bp
+from dotenv import load_dotenv
+import os
+
+# Load variables from .env (important for local dev)
+load_dotenv()
 
 app = Flask(__name__)
 CORS(app)
 
-# Replace with your actual MongoDB URI
-app.config['MONGO_URI'] = "mongodb+srv://prashid:pranusim0@cluster0.ha6azez.mongodb.net/"
+# Load Mongo URI from environment
+app.config['MONGO_URI'] = os.getenv('MONGO_URI')
 
+# Register routes
 app.register_blueprint(auth_bp)
 app.register_blueprint(tree_bp)
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run(debug=True, host="0.0.0.0", port=10000)
